09f4ce505a389508e2a28b7cf3d7bcb07e354bab
Sivadheeraj5356 <sivadheerajethamsetti@gmail.com>
2024-12-30 22:33:10 +0530

    testimonials

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'images.unsplash.com', // Add unsplash domain
+      // Add other domains if needed
+    ],
+  },
+}
+
+module.exports = nextConfig
+  
